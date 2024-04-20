6ffad5d2617e9fba40fe6b1a2decf0f4e4d4f58c
Ömer Asaf BALIKÇI <balikciomerasaf@gmail.com>
2024-04-20 19:53:06 +0300

    product manager regulations

---
+++ sales-service/src/test/java/com/toyota/salesservice/SalesserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyota.salesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
